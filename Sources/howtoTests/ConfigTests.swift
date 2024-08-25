ad1b1900eef0bf75f1537e39a750a81d2617300e
ilaumjd <ilaumjd@gmail.com>
2024-08-25 10:50:54 +0700

    add config unit test

---
+++ Sources/howtoTests/ConfigTests.swift
@@ -0,0 +1,62 @@
+import XCTest
+@testable import howto
+
+class ConfigTests: XCTestCase {
+    
+    func testNewConfigWithGoogleEngine() {
+        let result = Config.new(engineType: "google", num: 5)
+        
+        switch result {
+        case .success(let config):
+            XCTAssert(config.engine is GoogleEngine)
+            XCTAssertEqual(config.num, 5)
+        case .failure:
+            XCTFail("Config creation should not fail for valid input")
+        }
+    }
+    
+    func testNewConfigWithBingEngine() {
+        let result = Config.new(engineType: "bing", num: 10)
+        
+        switch result {
+        case .success(let config):
+            XCTAssert(config.engine is BingEngine)
+            XCTAssertEqual(config.num, 10)
+        case .failure:
+            XCTFail("Config creation should not fail for valid input")
+        }
+    }
+    
+    func testNewConfigWithInvalidEngine() {
+        let result = Config.new(engineType: "yahoo", num: 5)
+        
+        switch result {
+        case .success:
+            XCTFail("Config creation should fail for invalid engine")
+        case .failure(let error):
+            XCTAssertEqual(error, .invalidSearchEngine)
+        }
+    }
+    
+    func testNewConfigCaseInsensitivity() {
+        let result = Config.new(engineType: "GoOgLe", num: 5)
+        
+        switch result {
+        case .success(let config):
+            XCTAssert(config.engine is GoogleEngine)
+        case .failure:
+            XCTFail("Config creation should not fail for case-insensitive input")
+        }
+    }
+    
+    func testNewConfigWithInvalidNumber() {
+        let result = Config.new(engineType: "google", num: 0)
+        
+        switch result {
+        case .success:
+            XCTFail("Config creation should fail for invalid number")
+        case .failure(let error):
+            XCTAssertEqual(error, .invalidNumber)
+        }
+    }
+}
